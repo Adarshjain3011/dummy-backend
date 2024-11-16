e42b80e94a487f640bdbf918f0cd51a32188484d
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-11-16 12:05:10 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,25 +2,35 @@
 const express = require('express');
 const app = express();
 
-const candidateData  = require("./data")
+const candidateData = require("./data")
 
 app.listen(5000);
 
-app.get("/",(req,res)=>{
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*', // Replace with your frontend URL
+    credentials: true // Allow sending cookies with cross-origin requests
+}));
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
 
 
     res.send("helow ");
 })
 
 app.get("/getCandidateData/:userEmail", (req, res) => {
-    
+
     const userEmail = req.params.userEmail;
 
     console.log(userEmail);
 
 
     const candidateDataFiltered = candidateData.filter(candidate => candidate.email === userEmail);
-    
+
     if (candidateDataFiltered.length > 0) {
         res.status(200).json(candidateDataFiltered[0]);
     } else {
